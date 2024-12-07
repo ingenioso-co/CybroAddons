818c18b34d82a75ecbb4654ae392deab251655fd
Cybrosys Technologies <risvana@cybrosys.info>
2024-12-07 16:03:30 +0530

    Dec 07: [UPDT] Manifest Updated 'product_variant_import'

--- product_variant_import/__manifest__.py
+++ product_variant_import/__manifest__.py
@@ -41,7 +41,7 @@
         'security/product_variant_import_groups.xml',
         'wizards/import_product_variant_views.xml',
     ],
-    'images': ['static/description/banner.png'],
+    'images': ['static/description/banner.jpg'],
     'license': 'LGPL-3',
     'installable': True,
     'auto_install': False,
